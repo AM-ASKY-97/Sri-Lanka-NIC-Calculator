5c42950e9b52a20fc39c1b386e5e850baa130b87
97PHPDeveloper <am.asky97@gmail.com>
2021-11-18 18:29:55 +0530

    gender

--- index.php
+++ index.php
@@ -15,220 +15,233 @@
         $(document).ready(function(){
             <?php 
                 
-                $years = 0;
-                $months =0;
-                $weeks =0;
-                $days = 0;
-                $mins = 0;
-                $seconds =0;
-                $hours =0;
-                $user_date =0;
-                $yaer = 0;
-                $month =0;
-                $day =0;
-                $m =0;
-                $d =0;
+                $years = "00";
+                $months ="00";
+                $weeks ="00";
+                $days = "00";
+                $mins = "00";
+                $seconds ="00";
+                $hours ="00";
+                $user_date ="00";
+                $yaer = "00";
+                $month ="00";
+                $day ="00";
+                $m ="00";
+                $d ="00";
+                $unic="Processing.....";
+                $gender ="Processing.....";
             ?>    
         });
     </script>
 </head>
   <body>
 
-  <?php 
-   if(isset($_POST['submit']))
-   {
-    $nic = $_POST['date'];
+  <?php
+        
+        if(isset($_POST['submit']))
+        {
+            
+            $nic = $_POST['nic'];
 
-    $len = strlen($nic);
+            $unic = $nic;
 
-    if ($len == 12)
-    {
-        $yaer = substr($nic,0,4);
-;
+            $len = strlen($nic);
+        
+            if ($len == 12)
+            {
+                $yaer = substr($nic,0,4);
+        
+                $month = substr ($nic,4,3);
+                if ($month >500)
+                {
+                    $month = $month - 500;
+                    $gender = "Female";
+                }
 
-        $month = substr ($nic,4,3);
-        if ($month >500)
-        {
-            $month = $month - 500;
-        }
-        if($month >=0 && $month <=31)
-        {
-            $day = $month - 31;
-            $month = 01;
-        }
-        else if ($month >=32 && $month <=59)
-        {
-            $day = $month - 32;
-            $month = 02;
-        }
-        else if ($month >=60 && $month <=90)
-        {
-            $day = $month - 60;
-            $month = 03;
-        }
-        else if ($month >=91 && $month <=120)
-        {
-            $day = $month - 91;
-            $month = 04;
-        }
-        else if ($month >=121 && $month <=151)
-        {
-            $day = $month - 121;
-            $month = 05;
-        }
+                else
+                {
+                    $gender ="Male";
+                }
+                if($month >=0 && $month <=31)
+                {
+                    $day = $month - 31;
+                    $month = 01;
+                }
+                else if ($month >=32 && $month <=59)
+                {
+                    $day = $month - 32;
+                    $month = 02;
+                }
+                else if ($month >=60 && $month <=90)
+                {
+                    $day = $month - 60;
+                    $month = 03;
+                }
+                else if ($month >=91 && $month <=120)
+                {
+                    $day = $month - 91;
+                    $month = 04;
+                }
+                else if ($month >=121 && $month <=151)
+                {
+                    $day = $month - 121;
+                    $month = 05;
+                }
+        
+                else if ($month >=152 && $month <=181)
+                {
+                    $day = $month - 152;
+                    $month = 06;
+                }
+                else if ($month >=182 && $month <=212)
+                {
+                    $day = $month - 182;
+                    $month = 07;
+                }
+                else if ($month >=213 && $month <=243)
+                {
+                    $day = $month - 213;
+                    $month = 8;
+                }
+                else if ($month >=244 && $month <=273)
+                {
+                    $day = $month - 244;
+                    $month =9;;
+                }
+                else if ($month >=274 && $month <=304)
+                {
+                    $day = $month - 274;
+                    $month = 10;
+                }
+                else if ($month >=305 && $month <=334)
+                {
+                    $day = $month - 305;
+                    $month = 11;
+                }
+                else if ($month >=335 && $month <=366)
+                {
+                    $day = $month - 335;
+                    $month = 12;
+                }
+           
+            }
+        
+            
+        
+            else if ($len == 10)
+            {
+                $oldnic = substr ($nic,0,2);
+                $yaer = $oldnic + 1900;
+        
+        
+                $month = substr ($nic,2,3);
+                if ($month >500)
+                {
+                    $month = $month - 500;
+                    $gender = "Female";
+                }
 
-        else if ($month >=152 && $month <=181)
-        {
-            $day = $month - 152;
-            $month = 06;
-        }
-        else if ($month >=182 && $month <=212)
-        {
-            $day = $month - 182;
-            $month = 07;
-        }
-        else if ($month >=213 && $month <=243)
-        {
-            $day = $month - 213;
-            $month = 8;
-        }
-        else if ($month >=244 && $month <=273)
-        {
-            $day = $month - 244;
-            $month =9;;
-        }
-        else if ($month >=274 && $month <=304)
-        {
-            $day = $month - 274;
-            $month = 10;
-        }
-        else if ($month >=305 && $month <=334)
-        {
-            $day = $month - 305;
-            $month = 11;
-        }
-        else if ($month >=335 && $month <=366)
-        {
-            $day = $month - 335;
-            $month = 12;
-        }
-   
-    }
+                else
+                {
+                    $gender ="Male";
+                }
+                if($month >=0 && $month <=31)
+                {
+                    $day = $month - 0;
+                    $month = 01;
+                }
+                else if ($month >=32 && $month <=59)
+                {
+                    $day = $month - 32;
+                    $month = 02;
+                }
+                else if ($month >=60 && $month <=90)
+                {
+                    $day = $month - 60;
+                    $month = 03;
+                }
+                else if ($month >=91 && $month <=120)
+                {
+                    $day = $month - 91;
+                    $month = 04;
+                }
+                else if ($month >=121 && $month <=151)
+                {
+                    $day = $month - 121;
+                    $month = 05;
+                }
+        
+                else if ($month >=152 && $month <=181)
+                {
+                    $day = $month - 152;
+                    $month = 06;
+                }
+                else if ($month >=182 && $month <=212)
+                {
+                    $day = $month - 182;
+                    $month = 07;
+                }
+                else if ($month >=213 && $month <=243)
+                {
+                    $day = $month - 213;
+                    $month = "08";
+                }
+                else if ($month >=244 && $month <=273)
+                {
+                    $day = $month - 244;
+                    $month = "09";
+                }
+                else if ($month >=274 && $month <=304)
+                {
+                    $day = $month - 274;
+                    $month = 10;
+                }
+                else if ($month >=305 && $month <=334)
+                {
+                    $day = $month - 305;
+                    $month = 11;
+                }
+                else if ($month >=335 && $month <=366)
+                {
+                    $day = $month - 335;
+                    $month = 12;
+                }
+            }
+        
+            else
+            {
+                echo "Wrong NIc Number";
+            }
+        
+            $hours_in_day   = 24;
+            $minutes_in_hour= 60;
+            $seconds_in_mins= 60;
+                                        
+            $birth_date     = new DateTime($yaer."-". $month."-". $day);
+            $current_date   = new DateTime();
+                        
+            $diff           = $birth_date->diff($current_date);
+                        
+                        
+            $years     = $diff->y ; 
+            $m         = $diff->m;
+            $d         = $diff->d;
+            $months    = ($diff->y * 12) + $diff->m ; 
+            $weeks     = floor($diff->days/7);
+            $days      = $diff->days; 
+            $hours     = $diff->h + ($diff->days * $hours_in_day); 
+            $mins      = $diff->h + ($diff->days * $hours_in_day * $minutes_in_hour) ; 
+            $seconds   = $diff->h + ($diff->days * $hours_in_day * $minutes_in_hour * $seconds_in_mins) ; 
 
-    
+        }
+    ?>
 
-    else if ($len == 10)
-    {
-        $oldnic = substr ($nic,0,2);
-        $yaer = $oldnic + 1900;
-
-
-        $month = substr ($nic,2,3);
-        if ($month >500)
-        {
-            $month = $month - 500;
-        }
-        if($month >=0 && $month <=31)
-        {
-            $day = $month - 0;
-            $month = 01;
-        }
-        else if ($month >=32 && $month <=59)
-        {
-            $day = $month - 32;
-            $month = 02;
-        }
-        else if ($month >=60 && $month <=90)
-        {
-            $day = $month - 60;
-            $month = 03;
-        }
-        else if ($month >=91 && $month <=120)
-        {
-            $day = $month - 91;
-            $month = 04;
-        }
-        else if ($month >=121 && $month <=151)
-        {
-            $day = $month - 121;
-            $month = 05;
-        }
-
-        else if ($month >=152 && $month <=181)
-        {
-            $day = $month - 152;
-            $month = 06;
-        }
-        else if ($month >=182 && $month <=212)
-        {
-            $day = $month - 182;
-            $month = 07;
-        }
-        else if ($month >=213 && $month <=243)
-        {
-            $day = $month - 213;
-            $month = "08";
-        }
-        else if ($month >=244 && $month <=273)
-        {
-            $day = $month - 244;
-            $month = "09";
-        }
-        else if ($month >=274 && $month <=304)
-        {
-            $day = $month - 274;
-            $month = 10;
-        }
-        else if ($month >=305 && $month <=334)
-        {
-            $day = $month - 305;
-            $month = 11;
-        }
-        else if ($month >=335 && $month <=366)
-        {
-            $day = $month - 335;
-            $month = 12;
-        }
-    }
-
-        else 
-        {
-            echo ("wrong NIc");
-        }
-   }
-
-        $user_date = $yaer. "-".$month."-".$day;
-
-	
-		$hours_in_day   = 24;
-        $minutes_in_hour= 60;
-        $seconds_in_mins= 60;
-                                
-        $birth_date     = new DateTime($user_date);
-        $current_date   = new DateTime();
-                
-        $diff           = $birth_date->diff($current_date);
-                
-                
-        $years     = $diff->y ; 
-        $m         = $diff->m;
-        $d         = $diff->d;
-        $months    = ($diff->y * 12) + $diff->m ; 
-        $weeks     = floor($diff->days/7);
-        $days      = $diff->days; 
-        $hours     = $diff->h + ($diff->days * $hours_in_day); 
-        $mins      = $diff->h + ($diff->days * $hours_in_day * $minutes_in_hour) ; 
-        $seconds   = $diff->h + ($diff->days * $hours_in_day * $minutes_in_hour * $seconds_in_mins) ; 
-
-
-?>
-      
    <div class="container " style="background-color: #6a46bc;">
        <div class="row">
           <div class="col mt-3">
               <form method="post">
             
-                <input type="text" class="form-control" name="date" id="" placeholder="Plese Enter Your NIC number" Required>
+                <input type="text" class="form-control" name="nic" id="" placeholder="Plese Enter Your NIC number" Required>
 
                 <div class="row ">
                     <div class="col mt-3">
@@ -241,6 +254,39 @@
        </div>
 
        <div class="row">
+
+            <div class="col-lg-6 mt-3">
+                <div class="card">
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item bg-dark text-white">
+                        <div class="row">
+                            <div class="col">Your nic number is: </div>
+                        </div></li>
+
+                        <div class="row text-center mt-3 mb-3">
+                            <div class="col"><?php echo $unic ?></div>
+                        </div>
+                    </ul>
+                </div>
+           </div>
+
+
+           <div class="col-lg-6 mt-3">
+                <div class="card">
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item bg-dark text-white">
+                        <div class="row">
+                            <div class="col">Gender : </div>
+                        </div></li>
+
+                        <div class="row text-center mt-3 mb-3">
+                            <div class="col"><?php echo $gender ?></div>
+                        </div>
+                    </ul>
+                </div>
+           </div>
+
+
            <div class="col-lg-6 mt-3">
                 <div class="card">
                     <ul class="list-group list-group-flush">
