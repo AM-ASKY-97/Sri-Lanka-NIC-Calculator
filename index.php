2c74e8852ab440ddd1a80e238fd7e61878895201
97PHPDeveloper <am.asky97@gmail.com>
2021-11-18 19:29:22 +0530

    change font

--- index.php
+++ index.php
@@ -6,6 +6,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Crimson+Text&display=swap" rel="stylesheet">
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -35,7 +39,7 @@
         });
     </script>
 </head>
-  <body>
+  <body style="font-family: 'Crimson Text', serif;">
 
   <?php
         
@@ -233,8 +237,7 @@
             $current_date   = new DateTime();
                         
             $diff           = $birth_date->diff($current_date);
-                        
-                        
+                                    
             $years     = $diff->y ; 
             $m         = $diff->m;
             $d         = $diff->d;
